0f7aa8c84c56801fef94f8e5ad09aecd8bfca1ed
side <dmplo@mail.ru>
2023-06-17 20:41:26 +0700

    add Lesson_3

---
+++ Lesson_3/1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return int.Parse(Console.ReadLine());
+}
+
+int GetPalyndrom(int userNumber)
+{
+    int number = 0;
+    while (userNumber > 0)
+    {
+        int i = userNumber % 10;
+        number = number * 10 + i;
+        userNumber /= 10;
+    }
+    return number;
+}
+bool ValidatePalyndrom(int valueOne, int valueTwo)
+{
+    if (valueOne == valueTwo) return true;
+    return false;
+}
+
+int userNumber = Prompt("Введите число");
+int palyndrom = GetPalyndrom(userNumber);
+if (ValidatePalyndrom(userNumber, palyndrom)) System.Console.WriteLine($"Число {userNumber} палиндром");   
+else System.Console.WriteLine($"Число {userNumber} не палиндром");   
+
---
+++ Lesson_3/2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} ");
+    return int.Parse(Console.ReadLine());
+}
+System.Console.WriteLine("Введите кординаты точки А:");
+int x1 = Prompt("x:");
+int y1 = Prompt("y:");
+int z1 = Prompt("z:");
+
+System.Console.WriteLine("Введите кординаты точки B:");
+int x2 = Prompt("x:");
+int y2 = Prompt("y:");
+int z2 = Prompt("z:");
+
+double distanceX = x2 - x1;
+double distanceY = y2 - y1;
+double distanceZ = z2 - z1;
+
+
+double distance = Math.Sqrt(distanceX * distanceX + distanceY * distanceY + distanceZ * distanceZ);
+System.Console.WriteLine($"Расстояние между точками A и B составляет {distance:f2}");
+
+
